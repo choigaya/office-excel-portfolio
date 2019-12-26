f2edea2895d9cc4fa54f5a23e71582bbcd856b21
Gaya Choi <woosung827@naver.com>
2019-12-26 16:11:50 +0900

    html->heroku

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'/html')));
+app.use(express.static(path.join(__dirname,'public/html')));
 
 app.get('/', function(req, res) { // 홈 부분페이지
-    res.sendFile(path.join(__dirname, '/html', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/html', 'index.html'));
 });
 
 app.get('/skill', function(req, res) {// 스킬 부분 페이지
-    res.sendFile(path.join(__dirname, '/html', 'skill.html'));
+    res.sendFile(path.join(__dirname, 'public/html', 'skill.html'));
 });
 
 app.get('/portfolio', function(req, res) {// 포트폴리오 부분 페이지
-    res.sendFile(path.join(__dirname, '/html', 'portfolio.html'));
+    res.sendFile(path.join(__dirname, 'public/html', 'portfolio.html'));
 });
 
 app.get('/about', function(req, res) { // about 부분 페이지
-    res.sendFile(path.join(__dirname, '/html', 'about.html'));
+    res.sendFile(path.join(__dirname, 'public/html', 'about.html'));
 });
 
 
